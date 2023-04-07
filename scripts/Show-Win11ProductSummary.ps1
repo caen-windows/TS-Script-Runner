508bfb4cbda3b09e54b3e3c379ad5f751fdd973b
Greg Baker <bakegreg@umich.edu>
2023-04-07 11:42:07 -0400

    Updating product summary to reflect changes in Product/Version labeling for CLSE2023/Win11

---
+++ scripts/Show-Win11ProductSummary.ps1
@@ -0,0 +1,25 @@
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+
+$Product = $tsenv.Value("CLSEBD")
+$ComputerName = $tsenv.Value("CAENComputerName")
+$Model = Get-WmiObject Win32_Computersystem | foreach-object {$_.Model}
+
+if ($tsenv.Value("ZTILinux") -eq "true"){
+	$reloadMode = "Dual Boot Linux"
+}
+else{
+	$reloadMode = "Single Boot Windows"
+}
+
+#Display summary of the task sequence
+$Message = "The product to install has been dynamically selected based on the Active Directory OU of the computer object. Summary: `n`nProduct:                   $product`nReload mode::     $reloadMode`nComputer Name:   $ComputerName `nModel:                     $Model`n`nThis box will automatically close in two minutes." 
+$SecondsToWait = 120 #amount of time before the box automatically closes
+$Title = "CAEN Product Summary"
+$Button = 0 #a single OK button (https://msdn.microsoft.com/en-us/library/x83z1d9f(v=vs.84).aspx)
+$Icon = 64 #an Information icon
+
+$TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
+$TSProgressUI.CloseProgressDialog()
+
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+(New-Object -ComObject Wscript.Shell).popup($Message,$SecondsToWait,$Title,$Button + $Icon)
