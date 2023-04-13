775d210b92cc43c8fc3335dd624862b8dddb75d8
Greg Baker <bakegreg@umich.edu>
2023-04-13 16:59:42 -0400

    Updated to default to stable distribution

--- scripts/Get-CLSEBDdistribution.ps1
+++ scripts/Get-CLSEBDdistribution.ps1
@@ -1,9 +1,10 @@
 $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
-$ComputerDN = $tsenv.Value("DistinguishedName")
+$ComputerDN = $tsenv.Value("DistinguishedName") #variable is set by get-nameandproduct.ps1
 
 $TestingOU = "*OU=Testing,OU=Windows 11,OU=CAEN Lab Software Environment,OU=CAEN Managed Desktops,OU=CAEN,OU=ENGIN,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu"
 $UnstableOU = "*OU=Unstable,OU=Windows 11,OU=CAEN Lab Software Environment,OU=CAEN Managed Desktops,OU=CAEN,OU=ENGIN,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu"
 
+$returnValue = "Stable" #default to stable OU
 if ($computerDN -like $TestingOU){
     $returnValue = "Testing"
 }
