cb56971b40b1ffd294c886e8bd385c1d1e975756
Greg Baker <bakegreg@umich.edu>
2023-04-21 11:08:00 -0400

    Now supports pre-set TS variables

--- scripts/Get-CLSEBDdistribution.ps1
+++ scripts/Get-CLSEBDdistribution.ps1
@@ -1,14 +1,21 @@
 $tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
 $ComputerDN = $tsenv.Value("DistinguishedName") #variable is set by get-nameandproduct.ps1
-
+$distribution = $tsenv.Value("CAEN_DISTRIBUTION")
 $TestingOU = "*OU=Testing,OU=Windows 11,OU=CAEN Lab Software Environment,OU=CAEN Managed Desktops,OU=CAEN,OU=ENGIN,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu"
 $UnstableOU = "*OU=Unstable,OU=Windows 11,OU=CAEN Lab Software Environment,OU=CAEN Managed Desktops,OU=CAEN,OU=ENGIN,OU=Organizations,OU=UMICH,DC=adsroot,DC=itcs,DC=umich,DC=edu"
+$distributions = @(`
+    "Stable",`
+    "Testing",`
+    "Unstable"
+)
 
-$returnValue = "Stable" #default to stable OU
-if ($computerDN -like $TestingOU){
-    $returnValue = "Testing"
+if(-not($distribution -in $distributions)){
+    $returnValue = "Stable" #default to stable OU
+    if ($computerDN -like $TestingOU){
+        $returnValue = "Testing"
+    }
+    elseif ($computerDN -like $UnstableOU){
+        $returnValue = "Unstable"
+    }
+    $tsenv.Value("CAEN_DISTRIBUTION") = $returnValue
 }
-elseif ($computerDN -like $UnstableOU){
-    $returnValue = "Unstable"
-}
-$tsenv.Value("CAEN_DISTRIBUTION") = $returnValue
