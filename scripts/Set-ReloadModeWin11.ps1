788f5a5493ac3b7eb671f570a1563a0e0c66dfea
Baker <bakegreg@UMICH.EDU>
2024-08-22 16:19:07 -0400

    Fix for LeaveLinux TS variable not being used

--- scripts/Set-ReloadModeWin11.ps1
+++ scripts/Set-ReloadModeWin11.ps1
@@ -2,12 +2,15 @@ $tsenv = new-object -comobject Microsoft.SMS.TSEnvironment
 $ztilinux = $tsenv.Value("ZTILinux")
 $leaveLinux = $tsenv.Value("LeaveLinux")
 if (($leaveLinux -ne "true") -and ($leaveLinux -ne "false")){
-	if ((test-path (join-path $env:TEMP -ChildPath "linuxexists.txt")) -and ($ztilinux -eq "true")){
+	$linuxexists = test-path (join-path $env:TEMP -ChildPath "linuxexists.txt")
+	if ($linuxexists -and ($ztilinux -eq "true")){
+		write-output "LeaveLinux not set to 'true' or 'false'. Current value [ $LeaveLinux ]"
 		$tsenv.Value('LeaveLinux') = "true"
 		#Close the TS UI temporarily
 		$TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
 		$TSProgressUI.CloseProgressDialog()
-
+		$leaveLinux = $tsenv.Value("LeaveLinux")
+		write-output "Displaying reload mode selection box because LeaveLinux not previously set to 'true' or 'false'. LeaveLinux now set to [ $leavelinux ]."
 		$SecondsToWait = 60 #amount of time before the box automatically closes
 		$Title = "Install Option"
 		$Button = 4 #a yes/no button option (https://msdn.microsoft.com/en-us/library/x83z1d9f(v=vs.84).aspx)
@@ -20,6 +23,14 @@ if (($leaveLinux -ne "true") -and ($leaveLinux -ne "false")){
 		if ($choice -eq 6){ #6 equals a 'Yes'
 			$tsenv.Value('LeaveLinux') = "false"
 		}
+		$leaveLinux = $tsenv.Value("LeaveLinux")
+		write-output "Selection box choice = [ $choice ]. LeaveLinux now equals [ $leavelinux ]."
 	}
+	else {
+		write-output "Skipping display of reload mode selection box. LeaveLinux = [ $leavelinux ]. ZTILinux = [ $ztilinux ]. LinuxExists = [ $linuxexists ]."
+	}
+}
+else{
+	write-output "Skipping display of reload mode selection box. LeaveLinux = [ $leavelinux ]."
 }
 	
