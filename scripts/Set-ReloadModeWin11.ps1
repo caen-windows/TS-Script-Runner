2e7a0af6dac74bcea1cd1d94154530d78f9b2eb5
Baker <bakegreg@UMICH.EDU>
2024-08-22 10:16:48 -0400

    Fix for LeaveLinux TS variable not being used

--- scripts/Set-ReloadModeWin11.ps1
+++ scripts/Set-ReloadModeWin11.ps1
@@ -1,22 +1,25 @@
 $tsenv = new-object -comobject Microsoft.SMS.TSEnvironment
 $ztilinux = $tsenv.Value("ZTILinux")
-if ((test-path (join-path $env:TEMP -ChildPath "linuxexists.txt")) -and ($ztilinux -eq "true")){
-	$tsenv.Value('LeaveLinux') = "true"
-	#Close the TS UI temporarily
-	$TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
-	$TSProgressUI.CloseProgressDialog()
+$leaveLinux = $tsenv.Value("LeaveLinux")
+if (($leaveLinux -ne "true") -and ($leaveLinux -ne "false")){
+	if ((test-path (join-path $env:TEMP -ChildPath "linuxexists.txt")) -and ($ztilinux -eq "true")){
+		$tsenv.Value('LeaveLinux') = "true"
+		#Close the TS UI temporarily
+		$TSProgressUI = New-Object -COMObject Microsoft.SMS.TSProgressUI
+		$TSProgressUI.CloseProgressDialog()
 
-	$SecondsToWait = 60 #amount of time before the box automatically closes
-	$Title = "Install Option"
-	$Button = 4 #a yes/no button option (https://msdn.microsoft.com/en-us/library/x83z1d9f(v=vs.84).aspx)
-	$Icon = 64 #an Information icon
-	$Message = "Refresh both Windows and Linux?`n`nYes = Windows and Linux are refreshed.`nNo = Only Windows is refreshed."
-	$DefaultToNo = 256
+		$SecondsToWait = 60 #amount of time before the box automatically closes
+		$Title = "Install Option"
+		$Button = 4 #a yes/no button option (https://msdn.microsoft.com/en-us/library/x83z1d9f(v=vs.84).aspx)
+		$Icon = 64 #an Information icon
+		$Message = "Refresh both Windows and Linux?`n`nYes = Windows and Linux are refreshed.`nNo = Only Windows is refreshed."
+		$DefaultToNo = 256
 
-	$choice = (New-Object -ComObject Wscript.Shell).popup($Message,$SecondsToWait,$Title,$Button + $Icon + $DefaultToNo)
+		$choice = (New-Object -ComObject Wscript.Shell).popup($Message,$SecondsToWait,$Title,$Button + $Icon + $DefaultToNo)
 
-	if ($choice -eq 6){ #6 equals a 'Yes'
-		$tsenv.Value('LeaveLinux') = "false"
+		if ($choice -eq 6){ #6 equals a 'Yes'
+			$tsenv.Value('LeaveLinux') = "false"
+		}
 	}
 }
 	
