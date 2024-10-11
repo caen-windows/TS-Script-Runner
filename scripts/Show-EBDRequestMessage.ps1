f7601a268f793337554fcb93a488f34b5facca48
Baker <bakegreg@UMICH.EDU>
2024-10-11 10:45:57 -0400

    Text change to end user message

--- scripts/Show-EBDRequestMessage.ps1
+++ scripts/Show-EBDRequestMessage.ps1
@@ -2,7 +2,7 @@ $mac = get-ciminstance win32_networkadapter | where {$_.AdapterTypeId -eq 0 -and
 $smbiosguid = get-wmiobject Win32_ComputerSystemProduct  | Select-Object -ExpandProperty UUID
 
 #Display summary of the task sequence
-$Message = "The Windows 10 EBD is now available by request only. Please contact caen@umich.edu with the computer name, MAC address, and SMBIOS GUID of the system, as well as the reason Windows 10 is required. `n`nIn October 2025 Windows 10 reaches end of support from Microsoft. All Windows 10 EBD systems will need to be decommissioned or reloaded with Windows 11 by then.`n`nYour system information:`nMAC Address: [ $mac ]`nSMBIOS GUID: [ $smbiosguid ]" 
+$Message = "The Windows 10 EBD is available by request only. Please contact caen@umich.edu with the computer name, MAC address, SMBIOS GUID, and hardware model of the system, as well as the reason Windows 10 is required. `n`nIn October 2025 Windows 10 reaches end of support from Microsoft. All Windows 10 EBD systems must be decommissioned or reloaded with Windows 11 by then.`n`nYour system information:`nMAC Address: [ $mac ]`nSMBIOS GUID: [ $smbiosguid ]" 
 #$Message = "The Windows 10 EBD is now available by request only."
 $Title = "CAEN Notification"
 $Button = 0 #a single OK button (https://msdn.microsoft.com/en-us/library/x83z1d9f(v=vs.84).aspx)
