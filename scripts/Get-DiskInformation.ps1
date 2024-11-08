fffed78654fb20f604d61f4d10eb772f95b27a5e
Baker <bakegreg@UMICH.EDU>
2024-11-08 13:50:35 -0500

    Fixing formatting of message box

--- scripts/Get-DiskInformation.ps1
+++ scripts/Get-DiskInformation.ps1
@@ -11,7 +11,7 @@ if ($nonUsbDisks){
     }
     if ($diskinfo.count -ne 1){
         $diskinfostring = $diskinfo | out-string
-        $Message = "Warning: multiple disks detected. This can cause an installation failure or loss of data. Please disconnect all disks but the one you intend to install the OS to before continuing.`n$diskinfostring"
+        $Message = "Warning: multiple disks detected. This can cause an installation failure or loss of data on disk 0.`n`nPlease disconnect all disks but the one you intend to install the OS to and restart the install.`n$diskinfostring"
         $Message = $message.trim()
         
         $Title = "CAEN Notification"
